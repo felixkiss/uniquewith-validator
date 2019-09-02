827dbe676fbf9e5fd92bbe1f801baa4c42e3b54e
Erik Gaal <me@erikgaal.nl>
2019-09-02 15:22:56 +0200

    Add recent Laravel and PHP versions

--- scripts/change-laravel-version.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i 's/\"illuminate\/support\"\: \"5\.\*\"/\"illuminate\/support\"\: \"'$LARAVEL_VERSION'\"/' composer.json
-sed -i 's/\"illuminate\/validation\"\: \"5\.\*\"/\"illuminate\/validation\"\: \"'$LARAVEL_VERSION'\"/' composer.json
